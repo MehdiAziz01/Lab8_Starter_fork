5b197d98b7cc6ecb62d81ec777d444a5ef0195dd
MehdiAziz01 <azizmehdi001@gmail.com>
2025-05-28 03:51:08 -0700

    Implement Service Worker caching, PWA manifest, and update README

--- sw.js
+++ sw.js
@@ -3,13 +3,29 @@
 
 const CACHE_NAME = 'lab-8-starter';
 
+// URLs to cache
+const CACHED_URLS = [
+  '/',
+  '/index.html',
+  '/assets/scripts/main.js',
+  '/assets/css/style.css',
+  '/assets/images/icons/favicon.ico',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+];
+
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
+      // B6. Add all of the URLs from RECIPE_URLS here so that they are
       //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(CACHED_URLS);
     })
   );
 });
@@ -32,9 +48,31 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
-  // B7. TODO - Respond to the event by opening the cache using the name we gave
+  // B7. Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
+  // B8. If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache hit - fetch from network, add to cache and return network response
+        return fetch(event.request)
+          .then(networkResponse => {
+            if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+              return networkResponse;
+            }
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return networkResponse;
+          });
+      })
+  );
 });
